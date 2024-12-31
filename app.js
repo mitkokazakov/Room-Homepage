e9fa8713485be034d509f2cf599c17156c163c63
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-12-31 10:16:38 +0200

    Add overlay div with more darkness over the container when hamburger menu is clicked

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const burger = document.querySelector(".hamburger-container");
 const closeBtn = document.querySelector(".close-btn-container");
 
+const overlay = document.querySelector(".overlay");
+
 const mobileNav = document.querySelector(".mobile-navigation");
 
 burger.addEventListener("click", () => {
     mobileNav.classList.add("open")
+    overlay.classList.add("open")
 });
 
 closeBtn.addEventListener("click", () => {
     mobileNav.classList.remove("open")
+    overlay.classList.remove("open")
 });
